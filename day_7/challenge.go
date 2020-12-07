1581241881901ab27e24fc99cd95a46b103a0634
bbenzo <contact@benbarten.com>
2020-12-07 08:56:39 +0100

    wip: day 7

--- day_7/challenge.go
+++ day_7/challenge.go
@@ -16,13 +16,13 @@ type vertex struct {
 }
 
 type edge struct {
-	parents *vertex
+	parent *vertex
 	child   *vertex
 	weight  int
 }
 
 func main() {
-	file, err := os.Open("./day_6/input.txt")
+	file, err := os.Open("./day_7/input.txt")
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
@@ -34,26 +34,39 @@ func main() {
 		line := scanner.Text()
 		splits := strings.Split(line, " bags contain ")
 		if len(splits) < 2 {
-			log.Fatalf("invalid line in input: %v", line)
+			log.Fatalf("invalid line in input: %vert", line)
 		}
 
 		val := splits[0]
 
-		v := &vertex{val: val}
+		vert := &vertex{val: val}
 		_, ok := treeMap[val]
 		if strings.HasPrefix(splits[1], "contain no other") && !ok {
-			treeMap[val] = v
+			treeMap[val] = vert
 		}
 
-		regex := regexp.MustCompile("bag[s]*[.,]*\\s*")
+		regex := regexp.MustCompile("\\sbag[s]*[.,]*\\s*")
 		childrenStr := strings.Split(splits[1], "bag")
 		for _, childStr := range childrenStr {
 			weight, err := strconv.Atoi(string(childStr[0]))
 			if err != nil {
-				log.Fatalf("failed to parse num of child str: %v", childStr[0])
+				log.Fatalf("failed to parse num of child str: %vert", childStr[0])
 			}
 
+			childVal := childStr[1:]
 
+			matchingVertex, exists := treeMap[childVal]
+			if !exists {
+				childVertex := &vertex{
+					val: childVal,
+					parentEdges: []edge{{
+						parent: vert,
+						child:
+					}},
+				}
+
+				childVertex
+			}
 		}
 	}
 }
